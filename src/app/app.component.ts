bc39bf26fdd43757609ab4ab4fc5c79f86d35583
MarijanDupkovic <info@marijandupkovic.com>
2024-03-24 18:24:07 +0100

    Added sidenav with logic for switching with footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,7 @@
 import { HeaderComponent } from './components/header/header.component';
-import { Component, ElementRef, HostListener } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { UserDetailsComponent } from './components/board/user-details/user-details.component';
 @Component({
   selector: 'app-root',
   standalone: true,
--- src/app/components/board/board/board.component.ts
+++ src/app/components/board/board/board.component.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from '../footer/footer.component';
+import { PostHeaderComponent } from '../../header/post-header/post-header.component';
+import { CommonModule } from '@angular/common';
+import { SidebarComponent } from '../sidebar/sidebar.component';
 
 @Component({
   selector: 'app-board',
   standalone: true,
-  imports: [RouterOutlet, FooterComponent],
+  imports: [RouterOutlet, FooterComponent,PostHeaderComponent,CommonModule,SidebarComponent],
   templateUrl: './board.component.html',
   styleUrl: './board.component.scss'
 })
 export class BoardComponent {
+  showFooter = window.innerWidth < 1000;
+  constructor() { }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event:any) {
+    this.showFooter = event.target.innerWidth < 1000;
+  }
 
 }
