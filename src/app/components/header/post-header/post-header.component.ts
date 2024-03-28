b71b67e6a81347e87ff9d300d8e295ebda5646d2
MarijanDupkovic <info@marijandupkovic.com>
2024-03-28 23:20:21 +0100

    implemented colorcircle for post header

--- src/app/components/header/post-header/post-header.component.ts
+++ src/app/components/header/post-header/post-header.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { Component, HostListener } from '@angular/core';
 import { Router } from '@angular/router';
 import { SidebarComponent } from '../../board/sidebar/sidebar.component';
+import { ContactsService } from '../../../services/contacts.service';
 
 @Component({
   selector: 'app-post-header',
@@ -16,13 +17,24 @@ export class PostHeaderComponent {
   openMenu: boolean = false;
   user_mail: string = '';
   showFooter = window.innerWidth < 1000;
+  color_key: string = '';
 
-  constructor(private auth:AuthService,private route:Router) { }
+  constructor(private auth:AuthService,private route:Router,private contacts:ContactsService) { }
 
 
   ngOnInit() {
     this.auth.user_mail$.subscribe(usermail => {
       this.user_mail = usermail;
+    });
+    this.contacts.contacts$.subscribe(contacts => {
+      if (contacts.length > 0) {
+        contacts.forEach((contact:any) => {
+          if (contact.email === this.user_mail) {
+            this.short_name = contact.firstName.charAt(0) + contact.lastName.charAt(0);
+            this.color_key = contact.color_key;
+          }
+        });
+      }
     });
   }
 
