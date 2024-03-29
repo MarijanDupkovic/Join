de80fb43222816c91495963b244c487603875e6f
MarijanDupkovic <info@marijandupkovic.com>
2024-03-29 16:17:47 +0100

    fixed pre header animation and logo display on signup component

--- src/app/components/header/pre-header/pre-header.component.ts
+++ src/app/components/header/pre-header/pre-header.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Output } from '@angular/core';
+import { AfterViewInit, Component, EventEmitter, Output } from '@angular/core';
 import { AnimationServiceService } from '../../../services/animation-service.service';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-pre-header',
@@ -10,13 +11,26 @@ import { AnimationServiceService } from '../../../services/animation-service.ser
   styleUrl: './pre-header.component.scss'
 })
 export class PreHeaderComponent {
-
+  isSignIn: boolean = false;
   animationend: boolean = false;
-  constructor(private animationService:AnimationServiceService) { }
+  constructor(private animationService: AnimationServiceService, private router: Router) {
+    router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        if (event.urlAfterRedirects === '/login') {
+          this.isSignIn = event.urlAfterRedirects === '/login';
+        } else if (event.urlAfterRedirects === '/signup') {
+          console.log('signup');
+
+          this.animationend = true;
+        }
+      }
+    });
+  }
 
   onAnimationFinished() {
     this.animationService.animationFinished();
     this.animationend = true;
   }
+
 
 }
